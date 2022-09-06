5cbadc707f3bbcad83d338cb0d3e81fadde37a62
camilobelloHZ <cbelloh@unal.edu.co>
2022-09-06 02:33:02 -0500

    taller4.phyton

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -4,4 +4,4 @@ class Asignatura:
         self._nombre = nombre
         self._salon = salon
     def __str__(self):
-        return self._nombre+" "+self._salon
+        return self._nombre+ " " +self._salon
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,33 +1,33 @@
 
 
 from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
 
 
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def __str__(self):
-        return "Grupo de estudiantes: grupo predeterminado"
     def listadoAsignaturas(self, **kwargs):
-        if(self.asignatura is None):
-            self._asignaturas= list()
+        if(self._asignaturas is None):
+            self._asignaturas= []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     
-    def agregarAlumno(self, alumno, lista=None):
+    def agregarAlumno(self, alumno, lista=[]):
         if(lista is None):
-            lista= list()
+            lista.append(alumno)
+            self.listadoAlumnos = self.listadoAlumnos + lista
         else:
             self.listadoAlumnos = list(alumno)
-        lista.append(alumno)
-        self.listadoAlumnos = lista
+        
+
+    def __str__(self):
+        return str( ('Grupo de estudiantes: ' )+ (self._grupo))
  
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -38,5 +38,5 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
